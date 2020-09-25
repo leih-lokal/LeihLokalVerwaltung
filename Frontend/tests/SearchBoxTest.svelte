257c54edda8ce5233341a7133cfb4119939393f5
Daniel Geiger <Daniel.Geiger@fiduciagad.de>
2020-09-25 20:17:01 +0000

    test searchbox

---
+++ Frontend/tests/SearchBoxTest.svelte
@@ -0,0 +1,9 @@
+<script>
+  import WithSearchBox from "../src/components/Table/WithSearchBox";
+  let currentPage = 0;
+  export let preprocessedRows = [];
+</script>
+
+<WithSearchBox rows={preprocessedRows} let:rows={preprocessedFilteredRows} bind:currentPage>
+  {#each preprocessedFilteredRows as row}<a>{JSON.stringify(row)}</a>{/each}
+</WithSearchBox>
