608d086e18e9f0c484f7106d7d9a99ac69e0db87
Daniel Geiger <Daniel.Geiger@fiduciagad.de>
2020-09-26 15:14:40 +0000

    test reset of page to 0

--- Frontend/tests/SearchBox.test.js
+++ Frontend/tests/SearchBox.test.js
@@ -30,7 +30,7 @@ describe("Table Searchbox", () => {
             },
         });
 
-        let elements = Array.from(container.querySelectorAll("a"));
+        let elements = Array.from(container.querySelectorAll(".row"));
         expect(elements.length).toEqual(rows.length);
     });
 
@@ -43,7 +43,7 @@ describe("Table Searchbox", () => {
 
         const input = getByRole('textbox');
         await userEvent.type(input, '');
-        let elements = Array.from(container.querySelectorAll("a"));
+        let elements = Array.from(container.querySelectorAll(".row"));
         expect(elements.length).toEqual(rows.length);
     });
 
@@ -57,7 +57,7 @@ describe("Table Searchbox", () => {
         const input = getByRole('textbox');
         await userEvent.type(input, 'text not contained in any value');
 
-        let elements = Array.from(container.querySelectorAll("a"));
+        let elements = Array.from(container.querySelectorAll(".row"));
         expect(elements.length).toEqual(0);
     });
 
@@ -71,7 +71,7 @@ describe("Table Searchbox", () => {
         const input = getByRole('textbox');
         await userEvent.type(input, 'row2');
 
-        let elements = Array.from(container.querySelectorAll("a"));
+        let elements = Array.from(container.querySelectorAll(".row"));
         expect(elements.length).toEqual(1);
         expect(elements[0]).toHaveTextContent("row2_value1");
         expect(elements[0]).toHaveTextContent("row2_value2");
@@ -88,7 +88,7 @@ describe("Table Searchbox", () => {
         const input = getByRole('textbox');
         await userEvent.type(input, 'value1');
 
-        let elements = Array.from(container.querySelectorAll("a"));
+        let elements = Array.from(container.querySelectorAll(".row"));
         expect(elements.length).toEqual(3);
         expect(elements[0]).toHaveTextContent("row1_value1");
         expect(elements[1]).toHaveTextContent("row2_value1");
@@ -105,11 +105,11 @@ describe("Table Searchbox", () => {
         const input = getByRole('textbox');
 
         await userEvent.type(input, 'text not contained in any value');
-        let elements = Array.from(container.querySelectorAll("a"));
+        let elements = Array.from(container.querySelectorAll(".row"));
         expect(elements.length).toEqual(0);
 
         await userEvent.type(input, '{selectall}{del}');
-        elements = Array.from(container.querySelectorAll("a"));
+        elements = Array.from(container.querySelectorAll(".row"));
         expect(elements.length).toEqual(rows.length);
     });
 
@@ -123,11 +123,27 @@ describe("Table Searchbox", () => {
         const input = getByRole('textbox');
         await userEvent.type(input, 'row2_value2 row2_value1');
 
-        let elements = Array.from(container.querySelectorAll("a"));
+        let elements = Array.from(container.querySelectorAll(".row"));
         expect(elements.length).toEqual(1);
         expect(elements[0]).toHaveTextContent("row2_value1");
         expect(elements[0]).toHaveTextContent("row2_value2");
         expect(elements[0]).toHaveTextContent("row2_value3");
+    });
+
+    it("should set currentPage to 0 when searching", async () => {
+        const { container, getByRole } = render(SearchBoxTest, {
+            props: {
+                preprocessedRows: rows,
+                currentPage: 1
+            },
+        });
+
+        expect(container.querySelector(".currentPage")).toHaveTextContent("1");
+
+        const input = getByRole('textbox');
+        await userEvent.type(input, 'row');
+
+        expect(container.querySelector(".currentPage")).toHaveTextContent("0");
     });
 
 });
--- Frontend/tests/SearchBoxTest.svelte
+++ Frontend/tests/SearchBoxTest.svelte
@@ -1,9 +1,12 @@
 <script>
   import WithSearchBox from "../src/components/Table/WithSearchBox";
-  let currentPage = 0;
+  export let currentPage = 0;
   export let preprocessedRows = [];
 </script>
 
 <WithSearchBox rows={preprocessedRows} let:rows={preprocessedFilteredRows} bind:currentPage>
-  {#each preprocessedFilteredRows as row}<a>{JSON.stringify(row)}</a>{/each}
+  {#each preprocessedFilteredRows as row}
+    <div class="row">{JSON.stringify(row)}</div>
+  {/each}
+  <div class="currentPage">{currentPage}</div>
 </WithSearchBox>
