f6aa469528fb6ddfe7580b78cd44034b5c6fb679
Daniel Geiger <Daniel.Geiger@fiduciagad.de>
2020-10-01 19:29:11 +0000

    EditItemPopup

---
+++ Frontend/src/components/Items/EditItemPopup.svelte
@@ -0,0 +1,192 @@
+<script>
+  import { getContext, onDestroy } from "svelte";
+  import { ItemDatabase } from "../../database/Database.js";
+  import { saveParseTimestampToString, saveParseStringToTimeMillis } from "../../utils/utils.js";
+  import { notifier } from "@beyonk/svelte-notifications";
+  import Select from "svelte-select";
+
+  const { close } = getContext("simple-modal");
+
+  const status_on_website_options = [
+    { value: "deleted", label: "gelöscht" },
+    { value: "instock", label: "verfügbar" },
+    { value: "outofstock", label: "nicht verfügbar" },
+  ];
+  const status_on_website_option_labels = status_on_website_options.map((option) => option.label);
+
+  function convertInputsForDb() {
+    row.added = saveParseStringToTimeMillis(added_string);
+    row.status_on_website = status_on_website_options.find(
+      (option) => option.label === status_on_website_label?.label
+    )?.value;
+  }
+
+  function saveInDatabase() {
+    convertInputsForDb();
+    ItemDatabase.updateDoc(row)
+      .then((result) => notifier.success("Leihvorgang gespeichert!"))
+      .then(close)
+      .catch((error) => {
+        notifier.danger("Leihvorgang konnte nicht gespeichert werden!", 6000);
+        console.error(error);
+        close();
+      });
+  }
+
+  onDestroy(convertInputsForDb);
+
+  export let row;
+
+  let added_string = saveParseTimestampToString(row.added);
+  let status_on_website_label = status_on_website_options.find(
+    (option) => option.value === row.status_on_website
+  )?.label;
+</script>
+
+<style>
+  input[type="text"] {
+    width: 100%;
+    padding: 12px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    resize: vertical;
+  }
+
+  label {
+    padding: 12px 12px 12px 0;
+    display: inline-block;
+  }
+
+  .col-label {
+    float: left;
+    width: 35%;
+  }
+
+  .col-input {
+    float: left;
+    width: 65%;
+  }
+
+  .content {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    align-items: center;
+    flex: 1;
+    min-height: 2em;
+    padding-top: 20px;
+    padding-bottom: 20px;
+  }
+
+  .container {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  h1,
+  .footer {
+    height: 30px;
+    padding: 20px;
+    margin: 0;
+  }
+
+  .row {
+    width: 400px;
+    margin: 0 1rem;
+  }
+
+  .button-save {
+    float: right;
+    padding: 10px;
+  }
+
+  .button-cancel {
+    float: left;
+  }
+</style>
+
+<div class="container">
+  <h1>Gegenstand bearbeiten</h1>
+  <div class="content">
+    <div class="row">
+      <div class="col-label"><label for="item_id">Gegenstand Nr</label></div>
+      <div class="col-input">
+        <input type="text" id="item_id" name="item_id" value={row._id} disabled />
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-label"><label for="item_name">Gegenstand Name</label></div>
+      <div class="col-input">
+        <input type="text" id="item_name" name="item_name" bind:value={row.item_name} />
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-label"><label for="brand">Marke</label></div>
+      <div class="col-input">
+        <input type="text" id="brand" name="brand" bind:value={row.brand} />
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-label"><label for="itype">Typbezeichnung</label></div>
+      <div class="col-input">
+        <input type="text" id="itype" name="itype" bind:value={row.itype} />
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-label"><label for="category">Kategorie</label></div>
+      <div class="col-input">
+        <input type="text" id="category" name="category" bind:value={row.category} />
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-label"><label for="deposit">Pfand</label></div>
+      <div class="col-input">
+        <input type="text" id="deposit" name="deposit" bind:value={row.deposit} />
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-label"><label for="parts">Anzahl Teile</label></div>
+      <div class="col-input">
+        <input type="text" id="parts" name="parts" bind:value={row.parts} />
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-label"><label for="manual">Anleitung</label></div>
+      <div class="col-input">
+        <input type="text" id="manual" name="manual" bind:value={row.manual} />
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-label"><label for="package">Verpackung</label></div>
+      <div class="col-input">
+        <input type="text" id="package" name="package" bind:value={row.package} />
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-label"><label for="added">Erfasst am</label></div>
+      <div class="col-input">
+        <input type="text" id="added" name="added" bind:value={added_string} />
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-label"><label for="properties">Eigenschaften</label></div>
+      <div class="col-input">
+        <input type="text" id="properties" name="properties" bind:value={row.properties} />
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-label"><label for="status_on_website">Status auf Webseite</label></div>
+      <div class="col-input">
+        <Select
+          items={status_on_website_option_labels}
+          bind:selectedValue={status_on_website_label}
+          placeholder={'Auswählen...'} />
+      </div>
+    </div>
+  </div>
+  <div class="footer">
+    <button class="button-save" on:click={saveInDatabase}>Speichern</button>
+    <button class="button-cancel" on:click={close}>Abbrechen</button>
+  </div>
+</div>
--- Frontend/src/components/Items/Items.svelte
+++ Frontend/src/components/Items/Items.svelte
@@ -1,27 +1,8 @@
 <script>
+  import EditItemPopup from "./EditItemPopup.svelte";
   import { ItemDatabase } from "../../database/Database.js";
-  import { onMount, onDestroy } from "svelte";
+  import EditableDatabaseTable from "../Table/EditableDatabaseTable.svelte";
   import columns from "./Columns.js";
-  import Table from "../Table/Table.svelte";
-  import { notifier } from "@beyonk/svelte-notifications";
-
-  let rows = [];
-
-  ItemDatabase.fetchAllDocs()
-    .then((items) => (rows = items))
-    .catch((error) => {
-      console.error(error);
-      notifier.danger("Laden aus der Datenbank fehlgeschlagen!", 10000);
-    });
-
-  ItemDatabase.onChange((changedItems) => (rows = changedItems));
-
-  onMount(() => ItemDatabase.syncAndListenForChanges());
-  onDestroy(() => ItemDatabase.cancelSyncAndChangeListener());
-
-  function onRowClicked(item) {
-    console.log("click " + item);
-  }
 </script>
 
-<Table {rows} {columns} {onRowClicked} rowHeight={60} />
+<EditableDatabaseTable database={ItemDatabase} {columns} editPopup={EditItemPopup} />
