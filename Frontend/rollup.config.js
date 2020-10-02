af562a9318cc1e24de32eb4b9f508f46e818b760
Daniel Geiger <Daniel.Geiger@fiduciagad.de>
2020-10-02 16:08:53 +0000

    fix rollup css config

--- Frontend/rollup.config.js
+++ Frontend/rollup.config.js
@@ -28,12 +28,8 @@ export default {
       }),
     }),
     svelte({
-      // enable run-time checks when not in production
-      dev: !production,
-      // we'll extract any component CSS out into
-      // a separate file - better for performance
       css: (css) => {
-        css.write("public/build/bundle.css");
+        css.write("bundle.css");
       },
     }),
     copy({
