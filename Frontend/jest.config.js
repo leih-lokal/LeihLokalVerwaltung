a5c007ab8165ae78040915c3131891d4e2053289
Daniel Geiger <Daniel.Geiger@fiduciagad.de>
2020-10-03 22:54:41 +0200

    test configuration

--- Frontend/jest.config.js
+++ Frontend/jest.config.js
@@ -1,15 +1,16 @@
 module.exports = {
   transform: {
-    "^.+\\.js$": "babel-jest",
-    "^.+\\.svelte$": "svelte-jest",
+    "^.+\\.[t|j]sx?$": "babel-jest",
+    "\\.svelte$": ["svelte-jest"],
   },
-  moduleFileExtensions: ["js", "svelte", "json"],
-  testPathIgnorePatterns: ["node_modules"],
+  moduleFileExtensions: ["js", "svelte"],
   bail: false,
   verbose: true,
-  transformIgnorePatterns: [],
-  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect", "jest-svelte-events/extend-expect"],
+  transformIgnorePatterns: ["node_modules/(?!(svelte-checkbox)/)"],
+  setupFilesAfterEnv: [
+    "@testing-library/jest-dom/extend-expect",
+    "jest-svelte-events/extend-expect",
+  ],
   moduleDirectories: ["node_modules", "src", "tests"],
   modulePaths: ["<rootDir>", "<rootDir>/src", "<rootDir>/node_modules", "<rootDir>/tests"],
-  testEnvironment: "jest-environment-jsdom",
 };
