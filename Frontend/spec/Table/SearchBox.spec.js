1b06659bbfa36fb48a76060d8a1123dc08f3445f
Daniel Geiger <Daniel.Geiger@fiduciagad.de>
2020-10-16 14:55:16 +0000

    refactoring

--- Frontend/spec/Table/SearchBox.spec.js
+++ Frontend/spec/Table/SearchBox.spec.js
@@ -51,115 +51,105 @@ const columns = [
 ];
 
 describe("Table Searchbox", () => {
-  const renderSearchBox = async () => {
-    return render(SearchBoxTest, {
-      props: {
-        preprocessedRows: rows,
-        columns: columns,
-      },
-    });
-  };
-
-  const searchFor = async (searchText, container, getByRole) => {
-    const input = getByRole("textbox");
-
-    await userEvent.type(input, "{selectall}{del}");
-    await userEvent.type(input, searchText);
-    return Array.from(container.querySelectorAll(".row"));
-  };
-
-  const renderAndSearch = async (searchText) => {
-    const { container, getByRole } = await renderSearchBox();
-    return await searchFor(searchText, container, getByRole);
-  };
-
-  it("should not filter rows on initial render", () => {
-    const { container } = render(SearchBoxTest, {
-      props: {
-        preprocessedRows: rows,
-        columns: columns,
-      },
-    });
-
-    let elements = Array.from(container.querySelectorAll(".row"));
-    expect(elements.length).toEqual(rows.length);
-  });
-
-  it("should not filter rows for empty search term", async () => {
-    const tableRows = await renderAndSearch("");
-    expect(tableRows.length).toEqual(rows.length);
-  });
-
-  it("should filter values by not existing search term", async () => {
-    const tableRows = await renderAndSearch("text not contained in any value");
-    expect(tableRows.length).toEqual(0);
-  });
-
-  it("should filter values by search term contained in row2", async () => {
-    const tableRows = await renderAndSearch("row2");
-    expect(tableRows.length).toEqual(1);
-    expect(tableRows[0]).toHaveTextContent("row2_value1");
-    expect(tableRows[0]).toHaveTextContent("row2_value2");
-    expect(tableRows[0]).toHaveTextContent("row2_value3");
-  });
-
-  it("should filter values by search term contained in all rows", async () => {
-    const tableRows = await renderAndSearch("value1");
-    expect(tableRows.length).toEqual(3);
-    expect(tableRows[0]).toHaveTextContent("row1_value1");
-    expect(tableRows[1]).toHaveTextContent("row2_value1");
-    expect(tableRows[2]).toHaveTextContent("row3_value1");
-  });
-
-  it("should remove filter when deleting search term", async () => {
-    const { container, getByRole } = await renderSearchBox();
-
-    let tableRows = await searchFor("text not contained in any value", container, getByRole);
-    expect(tableRows.length).toEqual(0);
-
-    tableRows = await searchFor("", container, getByRole);
-    expect(tableRows.length).toEqual(rows.length);
-  });
-
-  it("should filter values by search term consisting of multiple values", async () => {
-    const tableRows = await renderAndSearch("row2_value2 row2_value1");
-    expect(tableRows.length).toEqual(1);
-    expect(tableRows[0]).toHaveTextContent("row2_value1");
-    expect(tableRows[0]).toHaveTextContent("row2_value2");
-    expect(tableRows[0]).toHaveTextContent("row2_value3");
-  });
-
-  it("should set currentPage to 0 when searching", async () => {
+  const renderSearchBox = async (currentPage = 0, searchText) => {
     const { container, getByRole } = render(SearchBoxTest, {
       props: {
         preprocessedRows: rows,
-        currentPage: 1,
         columns: columns,
+        currentPage: currentPage,
       },
     });
 
-    expect(container.querySelector(".currentPage")).toHaveTextContent("1");
-    await searchFor("row", container, getByRole);
-    expect(container.querySelector(".currentPage")).toHaveTextContent("0");
+    const input = getByRole("textbox");
+
+    const searchFor = async (text) => {
+      await userEvent.type(input, "{selectall}{del}");
+      await userEvent.type(input, text);
+    };
+
+    if (searchText) await searchFor(searchText);
+
+    return {
+      tableRows: () => Array.from(container.querySelectorAll(".row")),
+      searchFor: searchFor,
+      currentPageIndicator: () => container.querySelector(".currentPage"),
+    };
+  };
+
+  it("should not filter rows on initial render", async () => {
+    const { tableRows } = await renderSearchBox();
+    expect(tableRows().length).toEqual(rows.length);
+  });
+
+  it("should not filter rows for empty search term", async () => {
+    const { tableRows } = await renderSearchBox(0, "");
+    expect(tableRows().length).toEqual(rows.length);
+  });
+
+  it("should filter values by not existing search term", async () => {
+    const { tableRows } = await renderSearchBox(0, "text not contained in any value");
+    expect(tableRows().length).toEqual(0);
+  });
+
+  it("should filter values by search term contained in row2", async () => {
+    const { tableRows } = await renderSearchBox(0, "row2");
+    expect(tableRows().length).toEqual(1);
+    expect(tableRows()[0]).toHaveTextContent("row2_value1");
+    expect(tableRows()[0]).toHaveTextContent("row2_value2");
+    expect(tableRows()[0]).toHaveTextContent("row2_value3");
+  });
+
+  it("should filter values by search term contained in all rows", async () => {
+    const { tableRows } = await renderSearchBox(0, "value1");
+    expect(tableRows().length).toEqual(3);
+    expect(tableRows()[0]).toHaveTextContent("row1_value1");
+    expect(tableRows()[1]).toHaveTextContent("row2_value1");
+    expect(tableRows()[2]).toHaveTextContent("row3_value1");
+  });
+
+  it("should remove filter when deleting search term", async () => {
+    const { searchFor, tableRows } = await renderSearchBox();
+
+    await searchFor("text not contained in any value");
+    expect(tableRows().length).toEqual(0);
+
+    await searchFor("");
+    expect(tableRows().length).toEqual(rows.length);
+  });
+
+  it("should filter values by search term consisting of multiple values", async () => {
+    const { tableRows } = await renderSearchBox(0, "row2_value2 row2_value1");
+    expect(tableRows().length).toEqual(1);
+    expect(tableRows()[0]).toHaveTextContent("row2_value1");
+    expect(tableRows()[0]).toHaveTextContent("row2_value2");
+    expect(tableRows()[0]).toHaveTextContent("row2_value3");
+  });
+
+  it("should set currentPage to 0 when searching", async () => {
+    const { searchFor, currentPageIndicator } = await renderSearchBox(1);
+
+    expect(currentPageIndicator()).toHaveTextContent("1");
+    await searchFor("row");
+    expect(currentPageIndicator()).toHaveTextContent("0");
   });
 
   it("should search column from beginning", async () => {
-    const { container, getByRole } = await renderSearchBox();
+    const { searchFor, tableRows } = await renderSearchBox();
 
-    let tableRows = await searchFor("value4", container, getByRole);
-    expect(tableRows.length).toEqual(0);
+    await searchFor("value4");
+    expect(tableRows().length).toEqual(0);
 
-    tableRows = await searchFor("from_beginning_", container, getByRole);
-    expect(tableRows.length).toEqual(3);
+    await searchFor("from_beginning_");
+    expect(tableRows().length).toEqual(3);
   });
 
   it("should exclude column as defined in columns", async () => {
-    const tableRows = await renderAndSearch("value5");
-    expect(tableRows.length).toEqual(0);
+    const { tableRows } = await renderSearchBox(0, "value5");
+    expect(tableRows().length).toEqual(0);
   });
 
   it("should exclude column not contained in columns", async () => {
-    const tableRows = await renderAndSearch("value6");
-    expect(tableRows.length).toEqual(0);
+    const { tableRows } = await renderSearchBox(0, "value6");
+    expect(tableRows().length).toEqual(0);
   });
 });
