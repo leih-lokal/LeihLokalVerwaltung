b3994908166cf0346bb28ca2ae1130db597b2232
Daniel Geiger <Daniel.Geiger@fiduciagad.de>
2020-10-19 19:55:12 +0000

    loading animation

--- Frontend/src/components/Table/Table.svelte
+++ Frontend/src/components/Table/Table.svelte
@@ -3,6 +3,7 @@
   import TableHeader from "./TableHeader.svelte";
   import WithSearchFilterBar from "./WithSearchFilterBar.svelte";
   import WithPagination from "./WithPagination.svelte";
+  import LoadingAnimation from "./LoadingAnimation.svelte";
   import { fade } from "svelte/transition";
 
   export let onRowClicked = (row) => {};
@@ -35,20 +36,28 @@
 </style>
 
 <div class="container">
-  <WithSearchFilterBar {columns} {rows} {filters} let:rows={filteredRows} bind:currentPage>
-    <div in:fade>
-      <WithPagination rows={filteredRows} let:rows={rowsOfCurrentPage} bind:currentPage {rowHeight}>
-        <table>
-          <TableHeader {columns} bind:rows={sortedUnprocessedRows} />
-          {#each rowsOfCurrentPage as row}
-            <TableRow
-              {columns}
-              item={row}
-              {rowHeight}
-              on:click={() => onRowClicked(rows.find((x) => x._id === row._id))} />
-          {/each}
-        </table>
-      </WithPagination>
-    </div>
-  </WithSearchFilterBar>
+  {#if rows.length === 0}
+    <LoadingAnimation />
+  {:else}
+    <WithSearchFilterBar {columns} {rows} {filters} let:rows={filteredRows} bind:currentPage>
+      <div in:fade>
+        <WithPagination
+          rows={filteredRows}
+          let:rows={rowsOfCurrentPage}
+          bind:currentPage
+          {rowHeight}>
+          <table>
+            <TableHeader {columns} bind:rows={sortedUnprocessedRows} />
+            {#each rowsOfCurrentPage as row}
+              <TableRow
+                {columns}
+                item={row}
+                {rowHeight}
+                on:click={() => onRowClicked(rows.find((x) => x._id === row._id))} />
+            {/each}
+          </table>
+        </WithPagination>
+      </div>
+    </WithSearchFilterBar>
+  {/if}
 </div>
