9c34e3dca09face2933b9fab8dbb6e2ace96e4b8
Daniel Geiger <Daniel.Geiger@fiduciagad.de>
2020-11-13 20:19:46 +0000

    do not remove row when wc fetch fails

--- Frontend/src/components/TableEditors/Items/Items.svelte
+++ Frontend/src/components/TableEditors/Items/Items.svelte
@@ -4,29 +4,19 @@
   import columns from "./Columns.js";
   import filters from "./Filters.js";
   import { itemDb } from "../../../utils/stores";
-  import { notifier } from "@beyonk/svelte-notifications";
   import WoocommerceClient from "../../Database/WoocommerceClient";
 
   const woocommerceClient = new WoocommerceClient();
 
   const processRowAfterLoad = (item) =>
-    woocommerceClient
-      .fetchItem(item.wc_id)
-      .then((result) => {
-        item.status_on_website = result["stock_status"];
-        item.deposit = parseInt(String(result["attributes"][0]["options"][0]).replace("€", ""));
-        if (result["images"].length > 0) item.image = result["images"][0]["src"];
-        item.wc_url = result["permalink"];
-        item.category = result["categories"][0]["name"];
-        return item;
-      })
-      .catch((error) => {
-        notifier.warning(
-          `Der Status von ${item.item_name} konnte nicht von der Webseite abgefragt werden!`,
-          6000
-        );
-        console.log(error);
-      });
+    woocommerceClient.fetchItem(item.wc_id).then((result) => {
+      item.status_on_website = result["stock_status"];
+      item.deposit = parseInt(String(result["attributes"][0]["options"][0]).replace("€", ""));
+      if (result["images"].length > 0) item.image = result["images"][0]["src"];
+      item.wc_url = result["permalink"];
+      item.category = result["categories"][0]["name"];
+      return item;
+    });
 </script>
 
 <DatabaseTableEditor
