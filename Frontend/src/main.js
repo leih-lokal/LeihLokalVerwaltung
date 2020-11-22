0882ab763a10d659907e79829a6f7181e21374ad
Daniel Geiger <Daniel.Geiger@fiduciagad.de>
2020-11-22 18:14:02 +0100

    make pwa

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,7 +1,22 @@
-import App from './components/App.svelte';
+import App from "./components/App.svelte";
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker.register("/service-worker.js").then(
+      function (registration) {
+        // Registration was successful
+        console.debug("ServiceWorker registration successful with scope: ", registration.scope);
+      },
+      function (err) {
+        // registration failed :(
+        console.error("ServiceWorker registration failed: ", err);
+      }
+    );
+  });
+}
 
 var app = new App({
-	target: document.body
+  target: document.body,
 });
 
 export default app;
