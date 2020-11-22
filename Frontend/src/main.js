57c855b19ad6fad41e272696ce46081d10dd915b
Daniel Geiger <Daniel.Geiger@fiduciagad.de>
2020-11-22 18:18:45 +0100

    sw path

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -2,7 +2,7 @@ import App from "./components/App.svelte";
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function () {
-    navigator.serviceWorker.register("/service-worker.js").then(
+    navigator.serviceWorker.register("service-worker.js").then(
       function (registration) {
         // Registration was successful
         console.debug("ServiceWorker registration successful with scope: ", registration.scope);
