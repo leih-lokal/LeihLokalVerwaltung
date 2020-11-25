3ce7ae688c02ddd4f0f022e1c3d6d341930c41fb
Daniel Geiger <Daniel.Geiger@fiduciagad.de>
2020-11-25 21:45:40 +0100

    backup script

--- ExcelCouchDbSync/backup_to_excel.sh
+++ ExcelCouchDbSync/backup_to_excel.sh
@@ -3,7 +3,7 @@ export COUCHDB_PASSWORD=password
 export COUCHDB_HOST=http://127.0.0.1:5984
 
 mkdir -p /home/pi/backups
-python3 /home/pi/LeihLokalVerwaltung/ExcelCouchDbSync/couchdb_to_excel.py /home/pi/backups/backup_$(date +%F_%H:%M:%S) > /home/pi/backups/backup_$(date +%F_%H:%M:%$
+python3 /home/pi/LeihLokalVerwaltung/ExcelCouchDbSync/couchdb_to_excel.py /home/pi/backups/backup_$(date +%F_%H:%M:%S) > /home/pi/backups/backup_$(date +%F_%H:%M:%S) 2>&1
 
 # delete backups older than 30 days
 find /home/pi/backups -type f -mtime +30 -exec rm -f {} \;
