149749f864a129a6a3ba2e8c7f5123c7fcb659a3
skjerns <14980558+skjerns@users.noreply.github.com>
2021-01-01 16:20:05 +0100

    change weekStart to monday

--- Frontend/src/components/Input/DateInput.svelte
+++ Frontend/src/components/Input/DateInput.svelte
@@ -41,6 +41,7 @@
   export let timeMillis;
   const TIMEZONE_OFFSET_MS = new Date().getTimezoneOffset() * 60000;
 
+  let weekStart = 1;
   let dateChosen = false;
   let isNone = !timeMillis || timeMillis === 0;
   let selected = isNone ? new Date() : new Date(timeMillis);
@@ -78,6 +79,7 @@
 </style>
 
 <Datepicker
+  bind:weekStart
   bind:selected
   bind:dateChosen
   {daysOfWeek}
