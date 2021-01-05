fca734862f150cba00e9a47c92de4b7e5735c8b9
Daniel Geiger <Daniel.Geiger@fiduciagad.de>
2021-01-05 20:14:58 +0100

    Revert "filter for hidden"
    This reverts commit b81f7ed5138aac1c552438b73cf9b54fe493a553.

--- Frontend/src/components/TableEditors/Rentals/RentalPopupFormular.svelte
+++ Frontend/src/components/TableEditors/Rentals/RentalPopupFormular.svelte
@@ -73,238 +73,221 @@
             "Pfand",
             "Mitarbeiter",
         ])
-        .setInputs(
-            [
-                {
-                    id: "item_id",
-                    label: "Nr",
-                    group: "Gegenstand",
-                    type: InputTypes.AUTOCOMPLETE,
-                    bindTo: { keyValueStoreKey: "currentDoc", attr: "item_id" },
-                    onChange: (selectedItem) => {
-                        keyValueStore.setValue("currentDoc", {
-                            ...$keyValueStore["currentDoc"],
-                            item_id: selectedItem._id,
-                            item_name: selectedItem.item_name,
-                            deposit: selectedItem.deposit,
-                        });
-                    },
-                    searchFunction: (searchTerm) =>
-                        $itemDb.fetchDocsBySelector(
-                            idStartsWithAndNotDeletedSelector(searchTerm),
-                            ["_id", "item_name", "deposit"]
+        .setInputs([
+            {
+                id: "item_id",
+                label: "Nr",
+                group: "Gegenstand",
+                type: InputTypes.AUTOCOMPLETE,
+                bindTo: { keyValueStoreKey: "currentDoc", attr: "item_id" },
+                onChange: (selectedItem) => {
+                    keyValueStore.setValue("currentDoc", {
+                        ...$keyValueStore["currentDoc"],
+                        item_id: selectedItem._id,
+                        item_name: selectedItem.item_name,
+                        deposit: selectedItem.deposit,
+                    });
+                },
+                searchFunction: (searchTerm) =>
+                    $itemDb.fetchDocsBySelector(
+                        idStartsWithAndNotDeletedSelector(searchTerm),
+                        ["_id", "item_name", "deposit"]
+                    ),
+                suggestionFormat: (id, item_name) => `${id}: ${item_name}`,
+                noResultsText: "Kein Gegenstand mit dieser Id",
+            },
+            {
+                id: "item_name",
+                label: "Name",
+                group: "Gegenstand",
+                type: InputTypes.AUTOCOMPLETE,
+                bindTo: { keyValueStoreKey: "currentDoc", attr: "item_name" },
+                onChange: (selectedItem) => {
+                    keyValueStore.setValue("currentDoc", {
+                        ...$keyValueStore["currentDoc"],
+                        item_id: selectedItem._id,
+                        item_name: selectedItem.item_name,
+                        deposit: selectedItem.deposit,
+                    });
+                },
+                searchFunction: (searchTerm) =>
+                    $itemDb.fetchDocsBySelector(
+                        attributeStartsWithIgnoreCaseAndNotDeletedSelector(
+                            "item_name",
+                            searchTerm
                         ),
-                    suggestionFormat: (id, item_name) => `${id}: ${item_name}`,
-                    noResultsText: "Kein Gegenstand mit dieser Id",
+                        ["_id", "item_name", "deposit"]
+                    ),
+                suggestionFormat: (id, item_name) => `${id}: ${item_name}`,
+                noResultsText: "Kein Gegenstand mit diesem Name",
+            },
+            {
+                id: "update_status_on_website",
+                label: "Status auf Webseite aktualisieren",
+                group: "Gegenstand",
+                bindTo: {
+                    keyValueStoreKey: "options",
+                    attr: "updateStatusOnWebsite",
                 },
-                {
-                    id: "item_name",
-                    label: "Name",
-                    group: "Gegenstand",
-                    type: InputTypes.AUTOCOMPLETE,
-                    bindTo: {
-                        keyValueStoreKey: "currentDoc",
-                        attr: "item_name",
-                    },
-                    onChange: (selectedItem) => {
-                        keyValueStore.setValue("currentDoc", {
-                            ...$keyValueStore["currentDoc"],
-                            item_id: selectedItem._id,
-                            item_name: selectedItem.item_name,
-                            deposit: selectedItem.deposit,
-                        });
-                    },
-                    searchFunction: (searchTerm) =>
-                        $itemDb.fetchDocsBySelector(
-                            attributeStartsWithIgnoreCaseAndNotDeletedSelector(
-                                "item_name",
-                                searchTerm
-                            ),
-                            ["_id", "item_name", "deposit"]
+                type: InputTypes.CHECKBOX,
+            },
+
+            {
+                id: "rented_on",
+                label: "Erfasst am",
+                group: "Zeitraum",
+                type: InputTypes.DATE,
+                bindTo: { keyValueStoreKey: "currentDoc", attr: "rented_on" },
+            },
+            {
+                id: "extended_on",
+                label: "Verlängert",
+                group: "Zeitraum",
+                hidden: createNew,
+                type: InputTypes.DATE,
+                bindTo: { keyValueStoreKey: "currentDoc", attr: "extended_on" },
+            },
+            {
+                id: "to_return_on",
+                label: "Zurückerwartet",
+                group: "Zeitraum",
+                type: InputTypes.DATE,
+                bindTo: {
+                    keyValueStoreKey: "currentDoc",
+                    attr: "to_return_on",
+                },
+            },
+            {
+                id: "returned_on",
+                label: "Zurückgegeben",
+                group: "Zeitraum",
+                hidden: createNew,
+                type: InputTypes.DATE,
+                bindTo: { keyValueStoreKey: "currentDoc", attr: "returned_on" },
+            },
+
+            {
+                id: "customer_id",
+                label: "Nr",
+                group: "Kunde",
+                type: InputTypes.AUTOCOMPLETE,
+                bindTo: { keyValueStoreKey: "currentDoc", attr: "customer_id" },
+                onChange: (selectedCustomer) => {
+                    keyValueStore.setValue("currentDoc", {
+                        ...$keyValueStore["currentDoc"],
+                        name: selectedCustomer.lastname,
+                        customer_id: selectedCustomer._id,
+                    });
+                },
+                searchFunction: (searchTerm) =>
+                    $customerDb.fetchDocsBySelector(
+                        idStartsWithSelector(searchTerm),
+                        ["_id", "firstname", "lastname"]
+                    ),
+                suggestionFormat: (id, firstname, lastname) =>
+                    `${id}: ${firstname} ${lastname}`,
+                noResultsText: "Kein Kunde mit dieser Id",
+            },
+            {
+                id: "customer_name",
+                label: "Name",
+                group: "Kunde",
+                type: InputTypes.AUTOCOMPLETE,
+                bindTo: { keyValueStoreKey: "currentDoc", attr: "name" },
+                onChange: (selectedCustomer) => {
+                    keyValueStore.setValue("currentDoc", {
+                        ...$keyValueStore["currentDoc"],
+                        name: selectedCustomer.lastname,
+                        customer_id: selectedCustomer._id,
+                    });
+                },
+                searchFunction: (searchTerm) =>
+                    $customerDb.fetchDocsBySelector(
+                        attributeStartsWithIgnoreCaseSelector(
+                            "lastname",
+                            searchTerm
                         ),
-                    suggestionFormat: (id, item_name) => `${id}: ${item_name}`,
-                    noResultsText: "Kein Gegenstand mit diesem Name",
-                },
-                {
-                    id: "update_status_on_website",
-                    label: "Status auf Webseite aktualisieren",
-                    group: "Gegenstand",
-                    bindTo: {
-                        keyValueStoreKey: "options",
-                        attr: "updateStatusOnWebsite",
-                    },
-                    type: InputTypes.CHECKBOX,
-                },
+                        ["_id", "firstname", "lastname"]
+                    ),
+                suggestionFormat: (id, firstname, lastname) =>
+                    `${id}: ${firstname} ${lastname}`,
+                noResultsText: "Kein Kunde mit diesem Name",
+            },
 
-                {
-                    id: "rented_on",
-                    label: "Erfasst am",
-                    group: "Zeitraum",
-                    type: InputTypes.DATE,
-                    bindTo: {
-                        keyValueStoreKey: "currentDoc",
-                        attr: "rented_on",
-                    },
+            {
+                id: "deposit",
+                label: "Pfand",
+                group: "Pfand",
+                type: InputTypes.TEXT,
+                bindTo: { keyValueStoreKey: "currentDoc", attr: "deposit" },
+            },
+            {
+                id: "deposit_returned",
+                label: "Pfand zurück",
+                group: "Pfand",
+                hidden: createNew,
+                type: InputTypes.TEXT,
+                bindTo: {
+                    keyValueStoreKey: "currentDoc",
+                    attr: "deposit_returned",
                 },
-                {
-                    id: "extended_on",
-                    label: "Verlängert",
-                    group: "Zeitraum",
-                    hidden: createNew,
-                    type: InputTypes.DATE,
-                    bindTo: {
-                        keyValueStoreKey: "currentDoc",
-                        attr: "extended_on",
-                    },
+            },
+            {
+                id: "deposit_retained",
+                label: "einbehalten",
+                group: "Pfand",
+                hidden: createNew,
+                type: InputTypes.TEXT,
+                bindTo: {
+                    keyValueStoreKey: "currentDoc",
+                    attr: "deposit_retained",
                 },
-                {
-                    id: "to_return_on",
-                    label: "Zurückerwartet",
-                    group: "Zeitraum",
-                    type: InputTypes.DATE,
-                    bindTo: {
-                        keyValueStoreKey: "currentDoc",
-                        attr: "to_return_on",
-                    },
+            },
+            {
+                id: "deposit_retainment_reason",
+                label: "Grund",
+                group: "Pfand",
+                hidden: createNew,
+                type: InputTypes.TEXT,
+                bindTo: {
+                    keyValueStoreKey: "currentDoc",
+                    attr: "deposit_retainment_reason",
                 },
-                {
-                    id: "returned_on",
-                    label: "Zurückgegeben",
-                    group: "Zeitraum",
-                    hidden: createNew,
-                    type: InputTypes.DATE,
-                    bindTo: {
-                        keyValueStoreKey: "currentDoc",
-                        attr: "returned_on",
-                    },
-                },
+            },
 
-                {
-                    id: "customer_id",
-                    label: "Nr",
-                    group: "Kunde",
-                    type: InputTypes.AUTOCOMPLETE,
-                    bindTo: {
-                        keyValueStoreKey: "currentDoc",
-                        attr: "customer_id",
-                    },
-                    onChange: (selectedCustomer) => {
-                        keyValueStore.setValue("currentDoc", {
-                            ...$keyValueStore["currentDoc"],
-                            name: selectedCustomer.lastname,
-                            customer_id: selectedCustomer._id,
-                        });
-                    },
-                    searchFunction: (searchTerm) =>
-                        $customerDb.fetchDocsBySelector(
-                            idStartsWithSelector(searchTerm),
-                            ["_id", "firstname", "lastname"]
-                        ),
-                    suggestionFormat: (id, firstname, lastname) =>
-                        `${id}: ${firstname} ${lastname}`,
-                    noResultsText: "Kein Kunde mit dieser Id",
+            {
+                id: "passing_out_employee",
+                label: "Ausgabe",
+                group: "Mitarbeiter",
+                type: InputTypes.TEXT,
+                bindTo: {
+                    keyValueStoreKey: "currentDoc",
+                    attr: "passing_out_employee",
                 },
-                {
-                    id: "customer_name",
-                    label: "Name",
-                    group: "Kunde",
-                    type: InputTypes.AUTOCOMPLETE,
-                    bindTo: { keyValueStoreKey: "currentDoc", attr: "name" },
-                    onChange: (selectedCustomer) => {
-                        keyValueStore.setValue("currentDoc", {
-                            ...$keyValueStore["currentDoc"],
-                            name: selectedCustomer.lastname,
-                            customer_id: selectedCustomer._id,
-                        });
-                    },
-                    searchFunction: (searchTerm) =>
-                        $customerDb.fetchDocsBySelector(
-                            attributeStartsWithIgnoreCaseSelector(
-                                "lastname",
-                                searchTerm
-                            ),
-                            ["_id", "firstname", "lastname"]
-                        ),
-                    suggestionFormat: (id, firstname, lastname) =>
-                        `${id}: ${firstname} ${lastname}`,
-                    noResultsText: "Kein Kunde mit diesem Name",
+            },
+            {
+                id: "receiving_employee",
+                label: "Rücknahme",
+                group: "Mitarbeiter",
+                hidden: createNew,
+                type: InputTypes.TEXT,
+                bindTo: {
+                    keyValueStoreKey: "currentDoc",
+                    attr: "receiving_employee",
                 },
-
-                {
-                    id: "deposit",
-                    label: "Pfand",
-                    group: "Pfand",
-                    type: InputTypes.TEXT,
-                    bindTo: { keyValueStoreKey: "currentDoc", attr: "deposit" },
+            },
+            {
+                id: "remark",
+                label: "Bemerkung",
+                group: "Mitarbeiter",
+                hidden: createNew,
+                type: InputTypes.TEXT,
+                bindTo: {
+                    keyValueStoreKey: "currentDoc",
+                    attr: "remark",
                 },
-                {
-                    id: "deposit_returned",
-                    label: "Pfand zurück",
-                    group: "Pfand",
-                    hidden: createNew,
-                    type: InputTypes.TEXT,
-                    bindTo: {
-                        keyValueStoreKey: "currentDoc",
-                        attr: "deposit_returned",
-                    },
-                },
-                {
-                    id: "deposit_retained",
-                    label: "einbehalten",
-                    group: "Pfand",
-                    hidden: createNew,
-                    type: InputTypes.TEXT,
-                    bindTo: {
-                        keyValueStoreKey: "currentDoc",
-                        attr: "deposit_retained",
-                    },
-                },
-                {
-                    id: "deposit_retainment_reason",
-                    label: "Grund",
-                    group: "Pfand",
-                    hidden: createNew,
-                    type: InputTypes.TEXT,
-                    bindTo: {
-                        keyValueStoreKey: "currentDoc",
-                        attr: "deposit_retainment_reason",
-                    },
-                },
-
-                {
-                    id: "passing_out_employee",
-                    label: "Ausgabe",
-                    group: "Mitarbeiter",
-                    type: InputTypes.TEXT,
-                    bindTo: {
-                        keyValueStoreKey: "currentDoc",
-                        attr: "passing_out_employee",
-                    },
-                },
-                {
-                    id: "receiving_employee",
-                    label: "Rücknahme",
-                    group: "Mitarbeiter",
-                    hidden: createNew,
-                    type: InputTypes.TEXT,
-                    bindTo: {
-                        keyValueStoreKey: "currentDoc",
-                        attr: "receiving_employee",
-                    },
-                },
-                {
-                    id: "remark",
-                    label: "Bemerkung",
-                    group: "Mitarbeiter",
-                    hidden: createNew,
-                    type: InputTypes.TEXT,
-                    bindTo: {
-                        keyValueStoreKey: "currentDoc",
-                        attr: "remark",
-                    },
-                },
-            ].filter((input) => !input.hidden)
-        );
+            },
+        ]);
 </script>
 
 <PopupFormular
