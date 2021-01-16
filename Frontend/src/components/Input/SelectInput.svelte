b89191e4465173d3f622b866637ff291ef2e331c
Daniel Geiger <Daniel.Geiger@fiduciagad.de>
2021-01-16 21:08:53 +0100

    do not display "undefined"

--- Frontend/src/components/Input/SelectInput.svelte
+++ Frontend/src/components/Input/SelectInput.svelte
@@ -1,59 +1,54 @@
 <script>
-    import Select from "svelte-select/src/Select.svelte";
+  import Select from "svelte-select/src/Select.svelte";
 
-    export let selectedValuesString = "";
-    export let selectionOptions = [];
-    export let isMulti = true;
-    export let isCreatable = true;
-    export let isClearable = true;
-    export let disabled = false;
+  export let selectedValuesString = "";
+  export let selectionOptions = [];
+  export let isMulti = true;
+  export let isCreatable = true;
+  export let isClearable = true;
+  export let disabled = false;
 
-    const valuesToOptions = (values) => {
-        return values.map(
-            (value) =>
-                selectionOptions.find(
-                    (item) => item === value || item.value === value
-                ) ?? value
-        );
-    };
+  const valuesToOptions = (values) => {
+    return values.map(
+      (value) => selectionOptions.find((item) => item === value || item.value === value) ?? value
+    );
+  };
 
-    const selectedValuesFromString = (valueString) => {
-        let selectedValues = [];
-        if (valueString !== "") {
-            selectedValues = valuesToOptions(valueString.split(", "));
-        }
-        if (!isMulti) {
-            selectedValues =
-                selectedValues.length === 0 ? {} : selectedValues[0];
-        }
-        return selectedValues;
-    };
+  const selectedValuesFromString = (valueString) => {
+    let selectedValues = [];
+    if (valueString !== "") {
+      selectedValues = valuesToOptions(valueString.split(", "));
+    }
+    if (!isMulti) {
+      selectedValues = selectedValues.length === 0 ? "" : selectedValues[0];
+    }
+    return selectedValues;
+  };
 
-    $: selectedValuesArray = selectedValuesFromString(selectedValuesString);
+  $: selectedValuesArray = selectedValuesFromString(selectedValuesString);
 </script>
 
-<style>
-    :global(.selectContainer > input) {
-        cursor: pointer !important;
-    }
-</style>
-
 <Select
-    items={selectionOptions}
-    bind:selectedValue={selectedValuesArray}
-    on:select={(event) => {
-        let selection = event.detail;
-        if (selection) {
-            if (!Array.isArray(selection)) selection = [selection];
-            selectedValuesString = selection
-                .map((item) => item.value)
-                .join(', ');
-        } else {
-            selectedValuesString = '';
-        }
-    }}
-    isDisabled={disabled}
-    {isMulti}
-    {isCreatable}
-    {isClearable}
-    placeholder={'Auswählen...'} />
+  items={selectionOptions}
+  bind:selectedValue={selectedValuesArray}
+  on:select={(event) => {
+    let selection = event.detail;
+    if (selection) {
+      if (!Array.isArray(selection)) selection = [selection];
+      selectedValuesString = selection.map((item) => item.value).join(", ");
+    } else {
+      selectedValuesString = "";
+    }
+  }}
+  isDisabled={disabled}
+  {isMulti}
+  {isCreatable}
+  {isClearable}
+  placeholder={"Auswählen..."}
+/>
+
+<style>
+  :global(.selectContainer > input) {
+    cursor: pointer !important;
+  }
+</style>
