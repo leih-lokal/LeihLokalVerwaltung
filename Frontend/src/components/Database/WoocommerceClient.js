7e94c2da3ec66200468d1f2e323b55db87109e0a
Daniel Geiger <Daniel.Geiger@fiduciagad.de>
2021-01-25 20:29:26 +0100

    sync wc description

--- Frontend/src/components/Database/WoocommerceClient.js
+++ Frontend/src/components/Database/WoocommerceClient.js
@@ -57,6 +57,7 @@ class WoocommerceClient {
   }
 
   _translateItemAttributesForWc(item) {
+    const hasSynonyms = item.symomyms && item.symomyms.trim().length > 0;
     return {
       name: item.item_name,
       sku: item._id,
@@ -75,7 +76,9 @@ class WoocommerceClient {
         .split(", ")
         .filter((category) => category in WC_CATEGORIES)
         .map((category) => WC_CATEGORIES[category]),
-      //short_description: `<div class="hidden">Art.Nr.: ${item._id}</div>${item.properties}`,
+      short_description: `<div class="hidden">Art.Nr.: ${item._id}</div>${item.description} ${
+        hasSynonyms ? `<small>(Synonyme: ${item.symomyms})</small>` : ""
+      }`,
       meta_data: [
         {
           key: "marke",
