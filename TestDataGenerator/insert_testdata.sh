c5e17189baa30c5aa6ced586afbcbe3ea6de6917
Daniel Geiger <Daniel.Geiger@fiduciagad.de>
2021-04-10 17:08:24 +0200

    generate test data

---
+++ TestDataGenerator/insert_testdata.sh
@@ -0,0 +1,12 @@
+host="couchdb"
+
+echo "hallo"
+
+# delete database
+curl -X DELETE http://user:password@${host}:5984/leihlokal
+
+# create database
+curl -X PUT http://user:password@${host}:5984/leihlokal
+
+# insert testdata
+curl -X POST -H "Content-type:application/json" http://user:password@${host}:5984/leihlokal/_bulk_docs -d @testdata.json
