a01a88e383987f72e7c3d97ea70189eeced741b8
Daniel Geiger <Daniel.Geiger@fiduciagad.de>
2021-05-01 21:54:16 +0200

    fix wait for testdata

--- TestDataGenerator/wait_for_testdata.sh
+++ TestDataGenerator/wait_for_testdata.sh
@@ -2,7 +2,7 @@
 
 i=0
 
-while ! curl http://user:password@127.0.0.1:5984/leihlokal | grep "\"doc_count\":300" > /dev/null;
+while ! curl http://user:password@127.0.0.1:5984/leihlokal_test | grep "\"doc_count\":300" > /dev/null;
 do
     if [[ "$i" -gt 29 ]]; then
         echo "timed out while waiting for testdata after 30s!"
