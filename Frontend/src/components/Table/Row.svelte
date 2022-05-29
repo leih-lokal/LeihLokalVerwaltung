7ef005c2eb1cb8b83bd72dfe6c5fa4185786fb9b
skjerns <14980558+skjerns@users.noreply.github.com>
2022-05-29 22:52:12 +0200

    initial tryout

--- Frontend/src/components/Table/Row.svelte
+++ Frontend/src/components/Table/Row.svelte
@@ -1,6 +1,8 @@
 <script>
   import Cell from "./Cell.svelte";
   import COLORS from "../Input/ColorDefs";
+  import Menu from "../ContextMenu/Menu.svelte";
+  import MenuOption from "../ContextMenu/MenuOption.svelte";
 
   export let columns = [];
   export let item = {};
@@ -30,9 +32,51 @@
       newColor && newColor != "" ? newColor : defaultBackgroundColor
     );
   });
+
+  function closeMenu() {
+    item.showMenu = false;
+  }
+
+  let pos = { x: 0, y: 0 };
+  async function onRightClick(e) {
+    pos = { x: e.clientX, y: e.clientY };
+    console.log("pos:", pos, item);
+
+    // first close all "open" menu's in all steps
+
+    //if (step.showMenu) {
+    //		step.showMenu = false;
+    //		//await new Promise(res => setTimeout(res, 100));
+    //}
+
+    item.showMenu = true;
+    console.log("pos:", pos);
+  }
+
+  function returnSelectedRental() {
+    let isRed = false;
+    let i = 0;
+    for (i = 0; i < steps.length; i++) {
+      steps[i].isRed = isRed;
+      if (steps[i] === step) {
+        isRed = true;
+      }
+    }
+  }
 </script>
 
-<tr on:click style={`height: ${rowHeight}px;`}>
+<Menu {...pos} on:click={closeMenu} on:clickoutside={closeMenu}>
+  <MenuOption
+    on:click={returnSelectedRental}
+    text="Als zurückgegeben markieren"
+  />
+</Menu>
+
+<tr
+  on:click
+  on:contextmenu|preventDefault={onRightClick}
+  style={`height: ${rowHeight}px;`}
+>
   {#each columns as col, i}
     <Cell
       {rowHeight}
