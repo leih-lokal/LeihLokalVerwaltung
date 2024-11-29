f051121e1807dc0e26b179a49bda273061e20e81
Ferdinand Mütsch <ferdinand@muetsch.io>
2024-11-29 15:14:51 +0100

    perf: speed up rental highlighting queries (see #521)

--- Frontend/src/data/rental/dataHooks.js
+++ Frontend/src/data/rental/dataHooks.js
@@ -7,9 +7,26 @@ export default {
         const itemHighlightResolvers = data.map(() => Promise.withResolvers())
         const customerHighlightResolvers = data.map(() => Promise.withResolvers())
 
+        function getSelectQuery(entityType, ids) {
+            ids.sort((a, b) => a - b)
+            return {
+                $and: [
+                    { type: entityType },
+                    {
+                        id: {
+                            $gte: ids.length ? ids[0] : 0,
+                            $lte: ids.length ? ids[ids.length - 1] : 999999,
+                            $or: ids,
+                        }
+                    }
+                ]
+            }
+        }
+
         async function fetchItemHighlights() {
             const itemIds = data.map(d => d.item_id)
-            const result = await Database.fetchDocsBySelector({ $and: [ { type: 'item', id: { $or: itemIds } } ] }, ['id', '_rev', 'highlight'], [], data.length * 10)
+            const select = getSelectQuery('item', itemIds)
+            const result = await Database.fetchDocsBySelector(select, ['id', '_rev', 'highlight'], [], data.length * 10)
             data.forEach((e, i) => {
                 itemHighlightResolvers[i].resolve(result.find(item => item.id === e.item_id)?.highlight || '')
             })
@@ -17,7 +34,8 @@ export default {
 
         async function fetchCustomerHighlights() {
             const customerIds = data.map(d => d.customer_id)
-            const result = await Database.fetchDocsBySelector({ $and: [ { type: 'customer', id: { $or: customerIds } } ] }, ['id', '_rev', 'highlight'], [], data.length * 10)
+            const select = getSelectQuery('customer', customerIds)
+            const result = await Database.fetchDocsBySelector(select, ['id', '_rev', 'highlight'], [], data.length * 10)
             data.forEach((e, i) => {
                 customerHighlightResolvers[i].resolve(result.find(customer => customer.id === e.customer_id)?.highlight || '')
             })
@@ -25,7 +43,7 @@ export default {
 
         fetchItemHighlights()  // intentionally not awaited!
         fetchCustomerHighlights()  // intentionally not awaited!
-        
+
         data.forEach((e, i) => {
             e.item_highlight = itemHighlightResolvers[i].promise
             e.customer_highlight = customerHighlightResolvers[i].promise
